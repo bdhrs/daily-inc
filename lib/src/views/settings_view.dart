b821b5e56dda1ed35d684fc8ae60d2d7f44552ce
bdhrs <bodhirasa@gmail.com>
2025-07-28 17:32:20 +0530

    move hide when completed to the app bar

--- lib/src/views/settings_view.dart
+++ lib/src/views/settings_view.dart
@@ -1,6 +1,5 @@
 import 'package:daily_inc/src/data/data_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:logging/logging.dart';
 import 'package:daily_inc/src/theme/color_palette.dart';
 
@@ -13,33 +12,7 @@ class SettingsView extends StatefulWidget {
 
 class _SettingsViewState extends State<SettingsView> {
   final _log = Logger('SettingsView');
-  bool _hideWhenDone = false;
   final DataManager _dataManager = DataManager();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadSettings();
-  }
-
-  Future<void> _loadSettings() async {
-    _log.info('Loading settings...');
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _hideWhenDone = prefs.getBool('hideWhenDone') ?? false;
-    });
-    _log.info('Settings loaded: hideWhenDone=$_hideWhenDone');
-  }
-
-  Future<void> _updateHideWhenDone(bool value) async {
-    _log.info('Updating hideWhenDone to: $value');
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('hideWhenDone', value);
-    setState(() {
-      _hideWhenDone = value;
-    });
-    _log.info('hideWhenDone updated successfully.');
-  }
 
   Future<void> _resetAllData() async {
     _log.info('Resetting all data...');
@@ -105,12 +78,6 @@ class _SettingsViewState extends State<SettingsView> {
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          SwitchListTile(
-            title: const Text('Hide Completed Items'),
-            value: _hideWhenDone,
-            onChanged: _updateHideWhenDone,
-          ),
-          const SizedBox(height: 32),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               backgroundColor: ColorPalette.warningOrange,
